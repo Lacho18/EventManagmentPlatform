5e286c32e1cdc0971006517789e52a171883dc64
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2024-09-04 00:22:30 +0300

    commit

--- src/components/Features/EventCreation.jsx
+++ src/components/Features/EventCreation.jsx
@@ -1,17 +1,41 @@
 import { changeSingleEventHandler } from "../../store/singleEventSlice";
-import eventStructure from "./eventStructure";
 import { useDispatch, useSelector } from "react-redux";
 import useFetch from "../../hooks/useFetch";
+
+/*
+  Vsichki poleta rabotqt
+  1. Napravi proverki za validnost na dannite na survura
+  2. Izobrazi suobshteniqta za greshka
+  3. Opravi lilaviq theme color
+  4. Napravi logikata za zakupuvaneto na bilet
+    4.1. ID na eventa da se dobavq v "willParticipate" masiva na potrebitelq
+    4.2. Da se dicrementira broq na mestata za eventa sled zakypka
+    4.3. Sumata ot kypeniq bilet da se dobavi kum "moneySpent" poleto na potrebitelq
+    4.4. Da se dobavi logika v koqto event ne moje da bude cuknat i izglejda po siv ili bezcveten ako veche nqma svobodni mesta
+*/
 
 export default function EventCreation() {
   const dispatch = useDispatch();
   const eventData = useSelector((state) => state.singleEvent.eventData);
   const color = useSelector((state) => state.themeColor.color);
-  const keys = Object.keys(eventStructure);
+  const keys = Object.keys(eventData);
 
-  function changeHandler(e) {
+  function changeHandler(e, index = -1) {
+    console.log(e.target.name);
     dispatch(
-      changeSingleEventHandler({ name: e.target.name, value: e.target.value })
+      changeSingleEventHandler({
+        name: e.target.name,
+        value: e.target.value,
+        index: index,
+      })
+    );
+  }
+
+  function addButtonHandler(field) {
+    let array = [...eventData[field]];
+    array.push("");
+    dispatch(
+      changeSingleEventHandler({ name: field, value: array, index: -1 })
     );
   }
 
@@ -36,9 +60,9 @@ export default function EventCreation() {
         </p>
         <form onSubmit={submitHandler}>
           {keys.map((key) => {
-            if (typeof eventStructure[key] === "date") {
+            if (eventData[key] instanceof Date) {
               return (
-                <div className="p-2 m-3 text-lg formGroup">
+                <div key={key} className="p-2 m-3 text-lg formGroup">
                   <label htmlFor={key}>
                     Enter <span className="font-bold">{key}</span>
                   </label>
@@ -52,17 +76,49 @@ export default function EventCreation() {
               );
             }
 
-            if (typeof eventStructure[key] === "object") {
-              let subKeys = Object.keys(eventStructure[key]);
+            if (Array.isArray(eventData[key])) {
+              return (
+                <div key={key} className="p-2 m-5 formGroup">
+                  <label htmlFor={key}>
+                    Enter event <span className="font-bold">{key}s</span>
+                  </label>
+                  {eventData[key].map((indexValue, index) => (
+                    <div key={index * 10}>
+                      <p>
+                        {key} {index + 1}
+                      </p>
+                      <div className="flex">
+                        <input
+                          type="text"
+                          className="p-2"
+                          name={key}
+                          placeholder={key + " " + (index + 1)}
+                          onChange={(e) => changeHandler(e, index)}
+                        />
+                        <button
+                          className="border-2 text-xl rounded p-2"
+                          onClick={() => addButtonHandler(key)}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              );
+            }
 
-              let component = subKeys.map((subKey) => (
-                <div className="p-2 m-5 subFormGroup">
+            if (typeof eventData[key] === "object") {
+              let subKeys = Object.keys(eventData[key]);
+
+              let component = subKeys.map((subKey, index) => (
+                <div key={index} className="p-2 m-5 subFormGroup">
                   <label htmlFor={subKey}>
                     Enter event <span className="font-bold">{subKey}</span>
                   </label>
                   <input
                     type="text"
-                    className="text-lg p-2"
+                    className="p-1"
                     name={subKey}
                     placeholder={subKey}
                     onChange={changeHandler}
@@ -70,11 +126,16 @@ export default function EventCreation() {
                 </div>
               ));
 
-              return component;
+              return (
+                <div key={key} className="p-3 border-l-2">
+                  <p className="font-bold">{key}</p>
+                  {component}
+                </div>
+              );
             }
 
             return (
-              <div className="p-2 m-5 formGroup">
+              <div key={key} className="p-2 m-5 formGroup">
                 <label htmlFor={key}>
                   Enter event <span className="font-bold">{key}</span>
                 </label>
--- src/store/singleEventSlice.js
+++ src/store/singleEventSlice.js
@@ -3,11 +3,32 @@ import { createSlice } from '@reduxjs/toolkit';
 const singleEventSlice = createSlice({
     name: 'singleEvent',
     initialState: {
-        eventData: {}
+        eventData: {
+            name: "",
+            description: "",
+            location: {
+                country: "",
+                town: "",
+                location: ""
+            },
+            duration: "",
+            price: 0,
+            organizer_ID: 0,
+            image: [""],
+            event_date: new Date,
+            places: 0
+        }
     },
     reducers: {
         changeSingleEventHandler: (state, action) => {
-            state.eventData[action.payload.name] = action.payload.value;
+            console.log(action.payload.index);
+            //If the inserted data is for array and it has to be specialized
+            if (Array.isArray(state.eventData[action.payload.name]) && action.payload.index !== -1) {
+                state.eventData[action.payload.name][action.payload.index] = action.payload.value;
+            }
+            else {
+                state.eventData[action.payload.name] = action.payload.value;
+            }
         }
     },
 });
